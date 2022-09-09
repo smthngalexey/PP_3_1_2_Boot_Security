7efc954b5a9ddb74df317b72af01c86e20600362
”Alexey <smthngtrrbl@gmail.com>
2022-09-09 21:37:35 +0500

    lastUpdate?

--- src/main/java/kulish/webapp/controllers/AdminController.java
+++ src/main/java/kulish/webapp/controllers/AdminController.java
@@ -1,38 +1,26 @@
 package kulish.webapp.controllers;
 
+
 import kulish.webapp.models.User;
 import kulish.webapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-public class UserController {
+public class AdminController {
 
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public String getUser(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        model.addAttribute("user", userService.loadUserByUsername(username));
-        return "/user";
-    }
-
-/*    @GetMapping("/admin")
+    @GetMapping("/admin")
     public String getAllUsers(Model model) {
         List<User> users = userService.getListUsers();
         model.addAttribute("users", users);
@@ -69,5 +57,4 @@ public class UserController {
         userService.updateUser(user);
         return "redirect:/admin";
     }
-    */
 }
